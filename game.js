4db4b7e928ad92ca14263f178fe7f57907c66e74
Sarah <sranzieri@fi.uba.ar>
2022-09-03 20:23:51 +0200

    v2.0

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
-let board = document.querySelector(".board");
-board.style.gridTemplateColumns = "repeat(16, 1fr)";
-board.style.gridTemplateRows = "repeat(16, 1fr)";
+let board = document.querySelector('.board');
+board.style.gridTemplateColumns = 'repeat(16, 1fr)';
+board.style.gridTemplateRows = 'repeat(16, 1fr)';
 
 for(let i = 0; i < 256; i++) {
     let square = document.createElement('div')
